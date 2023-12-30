39869fa091f917c17b5db1726bb51aee51f33724
Ihor Fedorchenko <ihor.fedorchenko.if@gmail.com>
2023-12-30 18:56:33 +0200

    Minor server fix

--- express-backend/controllers/messages.controller.js
+++
@@ -1,7 +0,0 @@
-exports.getMessages = (req, res) => {
-    try{
-        res.json(['first', 'second', 'third'])
-    } catch (err){
-        res.status(500).send(err.message)
-    }
-}
--- express-backend/routes/sprint.routes.js
+++ express-backend/routes/sprint.routes.js
@@ -5,7 +5,7 @@ let router = require('express').Router()
 
 router.post('/projects/:projectId/sprints', authenticateToken, SprintController.create)
 router.get('/projects/:projectId/sprints', authenticateToken, SprintController.findAll)
-router.get('/sprints/:sprintId/assignee/:assigneeId/tasks',  SprintController.findAllSprintTasksByStatus)
+router.get('/sprints/:sprintId/assignee/:assigneeId/tasks', authenticateToken, SprintController.findAllSprintTasksByStatus)
 router.delete('/sprints/:sprintId', authenticateToken, SprintController.destroy)
 router.patch('/projects/:projectId/nextSprint', authenticateToken, SprintController.setNextSprint)
 
